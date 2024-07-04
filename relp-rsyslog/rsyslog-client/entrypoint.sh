526d03d9e7dbe4097bbb63dd2a6c6858a9631f77
psammarco <pietro.sammarco@googlemail.com>
2024-07-04 11:16:17 +0000

    adjustments

--- relp-rsyslog/rsyslog-client/entrypoint.sh
+++ relp-rsyslog/rsyslog-client/entrypoint.sh
@@ -4,21 +4,23 @@
 RSYSLOGSRV_CENTRAL="${RSYSLOGSRV_CENTRAL:-127.0.0.1}"
 RSYSLOGSRV_SECONDARY="${RSYSLOGSRV_SECONDARY:-127.0.0.1}"
 
+# rsyslog.conf variable PATH
+rsyslogconf=/etc/rsyslog/rsyslog.conf
 
 # Rsyslog forward config
 echo "*.* action(type=\"omrelp\" target=${RSYSLOGSRV_CENTRAL} port=\"22514\" \
     tls=\"on\" \
     tls.caCert=\"/rsyslog-certs/ca-cert.pem\" \
-    tls.tlscfgcmd=\"CipherString=ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384 MinProtocol=TLSv1.2 MaxProtocol=TLSv1.3\")" >> /etc/rsyslog.conf
+    tls.tlscfgcmd=\"CipherString=ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384 MinProtocol=TLSv1.2 MaxProtocol=TLSv1.3\")" >> $rsyslogconf
 
 # Append configuration for secondary server to rsyslog.conf
 echo "*.* action(type=\"omrelp\" target=${RSYSLOGSRV_SECONDARY} port=\"22514\" \
     tls=\"on\" \
     tls.caCert=\"/rsyslog-certs/ca-cert.pem\" \
     tls.tlscfgcmd=\"CipherString=ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384 MinProtocol=TLSv1.2 MaxProtocol=TLSv1.3\" \
-    action.execOnlyWhenPreviousIsSuspended=\"on\")" >> /etc/rsyslog.conf
+    action.execOnlyWhenPreviousIsSuspended=\"on\")" >> $rsyslogconf
 
-echo "*.* action(type=\"omfile\" file=\"/var/log/localbuffer\" action.execOnlyWhenPreviousIsSuspended=\"on\")" >> /etc/rsyslog.conf
+echo "*.* action(type=\"omfile\" file=\"/var/log/localbuffer\" action.execOnlyWhenPreviousIsSuspended=\"on\")" >> $rsyslogconf
 
 # Start rsyslog service
-exec rsyslogd -n
+exec rsyslogd -n -f $rsyslogconf
